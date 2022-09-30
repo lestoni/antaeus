a135155e7f9508d03e4e9324e2bf095e6052ad55
Tonny Mutai <tonnimutai@gmail.com>
2022-09-30 13:32:29 +0300

    add Invoice Status model as dependency

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
@@ -7,6 +7,7 @@ package io.pleo.antaeus.core.services
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
 import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class InvoiceService(private val dal: AntaeusDal) {
     fun fetchAll(): List<Invoice> {
@@ -15,5 +16,21 @@ class InvoiceService(private val dal: AntaeusDal) {
 
     fun fetch(id: Int): Invoice {
         return dal.fetchInvoice(id) ?: throw InvoiceNotFoundException(id)
+    }
+
+    fun setInvoiceAsPaid(id: Int): Invoice {
+        return dal.updateInvoiceStatus(id, InvoiceStatus.PAID, null) ?: throw InvoiceNotFoundException(id)
+    }
+
+    fun setInvoiceAsFailed(id: Int, failureReason: String): Invoice {
+        return dal.updateInvoiceStatus(id, InvoiceStatus.FAILED, failureReason) ?: throw InvoiceNotFoundException(id)
+    } 
+
+    fun setInvoiceForPaymentRetry(id: Int): Invoice {
+        return dal.updateInvoiceSettlementTries(id) ?: throw InvoiceNotFoundException(id)
+    }
+
+    fun fetchPendingInvoices(): List<Invoice> {
+        return dal.fetchInvoicesByStatus(InvoiceStatus.PENDING)
     }
 }
